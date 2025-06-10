1cd52ccfaad2e21e550834c73e935d8abb4eda04
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-06-10 12:59:20 -0500

    Create fahrenheit/celsius toggle.

--- src/displayController.js
+++ src/displayController.js
@@ -21,8 +21,79 @@ const displayController = (function () {
         img.src = url;
     }
 
-    PubSub.subscribe("gotGIF", insertGIF);
+    const changeLocation = function (location) {
+        const titleDiv = document.querySelector('.weather-data .title');
+        titleDiv.innerText = location;
+    }
 
+    const removeAllChildren = function (element) {
+        while (element.lastElementChild) {
+            element.removeChild(element.lastElementChild);
+        }
+    }
+
+    const createTempDiv = function (data) {
+        const { prefix, divClass, temp } = data;
+        const tempDiv = document.createElement("div");
+        const tempLabel = document.createTextNode(prefix);
+        const tempSpan = document.createElement('span');
+        tempSpan.innerText = temp;
+        const tempPostfix = document.createTextNode('\xB0');
+        tempDiv.appendChild(tempLabel);
+        tempDiv.appendChild(tempSpan);
+        tempDiv.appendChild(tempPostfix);
+        tempDiv.classList.add(divClass);
+        return tempDiv;
+
+    }
+
+    const insertWeatherData = function (_msg, data) {
+        const container = document.querySelector('.weather-details');
+        if (container.hasChildNodes()) {
+            removeAllChildren(container);
+        }
+        const conditions = document.createElement("div");
+        conditions.innerText = `Current conditions: ${data.conditions}`;
+        const tempDiv = createTempDiv({prefix: 'Temperature: ',
+            divClass: 'temp',
+            temp: data.temp,
+        });
+        const feelslike = createTempDiv({prefix: 'Feels like: ',
+            divClass: 'feelslike',
+            temp: data.feelslike,
+        });
+        const precipprob = document.createElement("div");
+        precipprob.innerText = `Chance of precipitation: ${data.precipprob}%`;
+        const humidity = document.createElement("div");
+        humidity.innerText = `Humidity: ${data.humidity}`;
+        container.appendChild(conditions);
+        container.appendChild(tempDiv);
+        container.appendChild(feelslike);
+        container.appendChild(precipprob);
+        container.appendChild(humidity);
+        changeLocation(data.location);
+    }
+
+    const changeTempSystem = function () {
+        const temp = document.querySelector('.temp > span').innerText;
+        const feelslike = document.querySelector('.feelslike > span').innerText;
+        PubSub.publish("changeTempSystem", {temp, feelslike});
+    }
+
+    const activateTempToggle = function () {
+        const toggle = document.querySelector('.temp-toggle span');
+        toggle.addEventListener("click", changeTempSystem);
+    }
+
+    const updateTemps = function (_msg, data) {
+        document.querySelector('.temp > span').innerText = data.temp;
+        document.querySelector('.feelslike > span').innerText = data.feelslike;
+    }
+
+    PubSub.subscribe("gotGIF", insertGIF);
+    PubSub.subscribe("gotWeather", insertWeatherData);
+    PubSub.subscribe("updateTemps", updateTemps);
+    activateTempToggle();
 
     return {
         activateSubmit,
--- src/weatherApp.js
+++ src/weatherApp.js
@@ -1,6 +1,12 @@
 import PubSub from 'pubsub-js';
 
 const weatherApp = (function() {
+    const API_KEY = "RQDHVRSVVH673VLFD6BX394TB"
+    const GIPHY_KEY = 'T5LsIn01axlEFNfHik4VPohk5Wz1no9W'
+    const tempSystem = {
+        value: 'fahrenheit',
+    }
+
     const catchErrors = function(fn) {
         return fn.catch((error) => {
             console.log(error);
@@ -13,19 +19,50 @@ const weatherApp = (function() {
             conditions: currentConditions.conditions, 
             feelslike: currentConditions.feelslike, 
             humidity: currentConditions.humidity, 
-            precipprob: currentConditions.precipprob };
+            precipprob: currentConditions.precipprob,
+            location: data.address,
+        };
     }
-    const API_KEY = "RQDHVRSVVH673VLFD6BX394TB"
-    // const url1 = 'https://api.giphy.com/v1/gifs/translate?api_key=T5LsIn01axlEFNfHik4VPohk5Wz1no9W&s=hamster&weirdness=10'
-    const GIPHY_KEY = 'T5LsIn01axlEFNfHik4VPohk5Wz1no9W'
+
+    const fahrenheitToCelsius = function (fahrenheit) {
+        return ((fahrenheit - 32) * 5/9).toFixed(1);
+    }
+
+    const celsiusToFahrenheit = function (celsius) {
+        return ((celsius * 9/5) + 32).toFixed(1);
+    }
+
+    const switchTemp = function (data) {
+        let temp;
+        let feelslike;
+        if (tempSystem.value === 'fahrenheit') {
+            temp = fahrenheitToCelsius(parseFloat(data.temp));
+            feelslike = fahrenheitToCelsius(parseFloat(data.feelslike));
+        } else if (tempSystem.value === 'celsius') {
+            temp = celsiusToFahrenheit(parseFloat(data.temp));
+            feelslike = celsiusToFahrenheit(parseFloat(data.feelslike));
+        }
+        PubSub.publish("updateTemps", {temp, feelslike});
+    }
+
+    const convertTemp = function (_msg, data) {
+        if (tempSystem.value === 'fahrenheit') {
+            switchTemp(data);
+            tempSystem.value = 'celsius'
+        } else if (tempSystem.value === 'celsius') {
+            switchTemp(data);
+            tempSystem.value = 'fahrenheit';
+        }
+    }
 
     const getWeather = async function (_msg, location) {
-        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=RQDHVRSVVH673VLFD6BX394TB`
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${API_KEY}`
         const response = await fetch(url, {mode: 'cors'});
         if (response.ok) {
             const data = await response.json();
             console.log(data);
             console.log(parseData(data));
+            PubSub.publish("gotWeather", parseData(data));
         } else {
             console.log("something went wrong");
         }
@@ -43,9 +80,10 @@ const weatherApp = (function() {
 
     }
 
-    getGIF('sunny');
+    catchErrors(getGIF('sunny'));
 
     PubSub.subscribe("searchStarted", getWeather);
+    PubSub.subscribe("changeTempSystem", convertTemp);
 
     return {
         getWeather,
